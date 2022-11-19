e388ceaf2e2ed18bc172f5ce5b268a9375fa3597
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-19 12:23:59 +0530

    modify admin.py file

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class AccountAdmin(UserAdmin):
+    list_display = ['email', 'username', 'last_login', 'is_admin']
+    search_fields = ['email', 'username']
+    readonly_fields = ['last_login']
 
-admin.site.register(Account)
+    filter_horizontal = []
+    list_filter = []
+    fieldsets = []
+
+
+admin.site.register(Account, AccountAdmin) 
--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from account.models import Account
 
 # Create your views here.
+
+def home_screen(request):
+    context = {}
+
+    accounts = Account.objects.all()
+    print(accounts)
+    context['accounts'] = accounts
+    return render(request,'home.html',context)
